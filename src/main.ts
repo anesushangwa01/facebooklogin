614172f0d9f5875717bbed60920c7d83bb44bdc8
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2024-12-07 12:46:16 +0200

    new

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideSocialAuth } from '@abacritt/angularx-social-login';
+import { FacebookLoginProvider } from '@abacritt/angularx-social-login';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  providers: [
+    provideHttpClient(),
+    provideSocialAuth({
+      autoLogin: false,
+      providers: [
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider('YOUR_FACEBOOK_APP_ID')
+        }
+      ]
+    })
+  ]
